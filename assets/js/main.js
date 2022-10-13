1d48b1b76cc5cae204d480fad679d3455f4edd55
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-13 15:17:08 +0200

    while loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,26 @@ const list = [
     'Frutta',
     'Pasta',
     'Pane',
-    'Latte'
+    'Latte',
+    'Caffè'
 ]
 
+// stampo gli elementi dell'array con while loop
+
+//  seleziono elemento della DOM
+const groceryListElement = document.querySelector('.grocery_list');
+
+console.log(groceryListElement);
+
+// ciclo while
+
+let i = 0;
+
+while (i < list.length) {
+    const listItem = list[i];
+
+    const groceryListItem = `<li><input type="checkbox" name="" id=""> ${listItem}</li>`;
+    groceryListElement.insertAdjacentHTML('beforeend', groceryListItem);
+
+    i++;
+}
